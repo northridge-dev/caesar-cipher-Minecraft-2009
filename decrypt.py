12f9d646dc773db3961f7712a9611e470005363a
Minecraft-2009 <124087796+Minecraft-2009@users.noreply.github.com>
2024-05-07 01:55:32 +0000

    commit in case codespace deleted

--- decrypt.py
+++ decrypt.py
@@ -1,16 +1,51 @@
 #!/usr/bin/env python
+from values import letter_values
+from time import sleep
+import os
 
 
-def decrypt(ciphertext, shift=0):
+def decrypt(message, shifter = None):
     """
     Decrypts a Caesar cipher encrypted message.
     :param ciphertext: The message to be encrypted
     :param shift: The number of positions to shift each letter
     """
-    return ciphertext
+    decoded_message = ""
+    if shifter != None:
+        if not shifter.isnumeric():
+            return "Input a proper shift you nitwit"
+        shifter = int(shifter)
+        for l in message:
+            n = (letter_values[l]-shifter)
+            if n <1:
+                n += 26
+            for k in letter_values.keys():
+                if n == letter_values[k]:
+                    decoded_message += k
+        return decoded_message
+
+    elif shifter == None:
+        nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
+        decodings = []
+        for shift in nums:
+            de_mess = ""
+            for l in message:
+                n = (letter_values[l]-shift)
+                if n <1:
+                    n += 26
+                for k in letter_values.keys():
+                    if n == letter_values[k]:
+                        de_mess += k
+            decodings.append(de_mess)
+        return decodings
+            
+            
+
+    # return ciphertext
 
 
 if __name__ == "__main__":
     ciphertext = input("Message to decrypt: ")
-    shift = int(input("Shift: "))
+    sleep(0.5)
+    shift = input("Shift: ")
     print(decrypt(ciphertext, shift))
